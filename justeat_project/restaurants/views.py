4ff3ac138da9ee7f88deb4af58667511654296bf
Venkatsnv01 <venkatsnv01@gmail.com>
2026-04-22 09:39:30 +0200

    implement form and removed pycache

--- justeat_project/restaurants/views.py
+++ justeat_project/restaurants/views.py
@@ -1,6 +1,19 @@
-from django.http import JsonResponse
+from django.shortcuts import render
+from .forms import PostcodeForm
 from .services import get_restaurants
 
 def restaurant_list(request):
-    restaurants = get_restaurants('SW1A1AA')
-    return JsonResponse(restaurants, safe=False)
+    restaurants = None
+
+    if request.method == 'POST':
+        form = PostcodeForm(request.POST)
+        if form.is_valid():
+            postcode = form.cleaned_data['postcode']
+            restaurants = get_restaurants(postcode)
+    else:
+        form = PostcodeForm()
+
+    return render(request, 'restaurants/restaurant_list.html', {
+        'form': form,
+        'restaurants': restaurants,
+    })
